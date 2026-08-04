7f0f461bdea9d883fd80ef2fec66779571a5538a
graceobonyo <gjobonyo@gmail.com>
2026-08-04 18:12:05 +0100

    README.md

--- health.py
+++ health.py
@@ -4,8 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # 1. Load your separate raw files once when the server starts
-data1 = pd.read_csv('cleaned_health_dataset.csv') 
-data2 = pd.read_csv('country_metadata.csv')       
+data1 = pd.read_csv('cleaned_Health_indicator_data.csv') 
+data2 = pd.read_csv('cleaned_country_data.csv') 
+data      
 
 # 2. Clean column typography layout
 data1.columns = data1.columns.str.strip().str.lower()
@@ -13,12 +14,12 @@ data2.columns = data2.columns.str.strip().str.lower()
 
 # 3. Perform the Master Merge 
 # This attaches country_name, region, and income_level to every health record
-master_df = pd.merge(data1, data2, on='country_code', how='left')
+merged_df = pd.merge(data1, data2, on='country_code', how='left')
 
 @app.get("/api/dashboard/{country_code}")
 def get_dashboard_data(country_code: str):
     # Filter the master merged dataset for the requested country
-    country_df = master_df[master_df['country_code'] == country_code.upper()]
+    country_df = merged_df[merged_df['country_code'] == country_code.upper()]
     
     # Sort chronologically so your line and stacked bar charts animate correctly
     country_df = country_df.sort_values(by='year')
